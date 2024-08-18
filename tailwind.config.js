156759cdfa0bc36ce306d39ae5e3b78af00d0755
myudro33 <qanashvilinika7@gmail.com>
2024-08-18 14:46:08 +0400

    style: add color setup in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {},
+    colors:{
+      "white": "hsl(0, 0%, 100%)",
+      "light-gray":"hsl(217, 12%, 63%)",
+      "dark-blue":" hsl(213, 19%, 18%)",
+      "very-dark-blue":"hsl(216, 12%, 8%)"
+    }
   },
   plugins: [],
 }
