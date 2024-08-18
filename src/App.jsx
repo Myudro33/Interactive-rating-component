5ed0b7be67a8d5527ea47f124964771742eb48f2
myudro33 <qanashvilinika7@gmail.com>
2024-08-18 23:10:03 +0400

    feat: add wrapper div for component container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import ContainerComponent from "./components/ContainerComponent"
+
 function App() {
 
   return (
-  <div>
-
+  <div className="w-screen h-screen bg-very-dark-blue flex items-center justify-center">
+    <ContainerComponent/>
   </div>
   )
 }
